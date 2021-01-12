caa52bf5095dbd36bbd7d8ad09c138337fd7f0fc
Anjaneya Tripathi <anjaneya532000@gmail.com>
2021-01-12 16:02:52 +0530

    modified team preference

--- src/overview.py
+++ src/overview.py
@@ -7,7 +7,10 @@ def overview():
     r = requests.get(URL) 
     soup = BeautifulSoup(r.content, 'html5lib') 
     table = soup.find_all('li', attrs = {'class': 'cb-col cb-col-25 cb-mtch-blk cb-vid-sml-card-api videos-carousal-item cb-carousal-item-large cb-view-all-ga'}) 
-    pref = 'HBH'
+    
+    # replace this with the team initials eg. IND for India
+    pref = 'team-initials'
+    
     for li in table:
         match = li.text.strip()
         if(match.count(pref)>0):
